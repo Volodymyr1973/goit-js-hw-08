0d2edeacbf24a9f50a205888b8b8b3225a9051ee
Volodymyr <104526892+Volodymyr1973@users.noreply.github.com>
2022-10-04 22:35:58 +0300

    task 2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,74 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', event => {
+  console.log(event.seconds);
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(event.seconds)
+  );
+
+  let forTime = +localStorage.getItem('videoplayer-current-time');
+  console.log(forTime);
+
+  console.dir(player.setCurrentTime(forTime));
+
+  // player
+  //   .setCurrentTime(forTime)
+  //   .then(function (seconds) {
+  //     console.log('player');
+  //     // seconds = the actual time that the player seeked to
+  //   })
+  //   .catch(function (error) {
+  //     switch (error.name) {
+  //       case 'RangeError':
+  //         // the time was less than 0 or greater than the video’s duration
+  //         break;
+
+  //       default:
+  //         // some other error occurred
+  //         break;
+  //     }
+  //   });
+});
+
+console.dir(player);
+console.dir(iframe);
+
+// //on(event: string, callback: function)
+// Add an event listener for the specified event. Will call the callback with a single parameter,
+// data, that contains the data for that event.See events below for details.
+
+// const onPlay = function (data) {
+// data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
+
+// timeupdate
+// Triggered as the currentTime of the video updates. It generally fires every 250ms, but it may vary depending on the browser.
+
+// {
+//     duration: 61.857
+//     percent: 0.049
+//     seconds: 3.034
+// }
+
+// player
+//   .setCurrentTime(30.456)
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,59 @@
+console.log('Hello World');
 
+const inputEl = document.querySelector('.feedback-form input');
+console.dir(inputEl);
+
+const textareaEl = document.querySelector('.feedback-form textarea');
+console.dir(textareaEl);
+
+const btnEl = document.querySelector(`.feedback-form button`);
+console.log(btnEl);
+
+const formEl = document.querySelector('.feedback-form');
+console.dir(formEl);
+
+let emailIn;
+
+let textIn;
+
+let arr = {};
+
+const onInput = event => {
+  event.preventDefault();
+  console.log(event);
+  console.log(event.path[0].type);
+  if (event.path[0].type === 'email') {
+    emailIn = event.target.value;
+    arr.email = emailIn;
+
+    console.log(emailIn);
+  } else if (event.path[0].type === 'textarea') {
+    textIn = event.target.value;
+    arr.message = textIn;
+
+    console.log(textIn);
+  }
+
+  //   let arr = { email: `${emailIn}`, text: `${textIn}` };
+  console.log(arr);
+  localStorage.setItem('feedback-form-state', JSON.stringify(arr));
+};
+
+formEl.addEventListener('input', onInput);
+
+const onSubmit = event => {
+  event.preventDefault();
+  console.log(event.currentTarget);
+  const email = event.target[0].value;
+  console.dir(email);
+  const textarea = event.target[1].value;
+  console.log(textarea);
+  let arr = { email: `${email}`, text: `${textarea}` };
+  console.log(arr);
+  localStorage.setItem('feedback-form-state', JSON.stringify(arr));
+  event.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+};
+
+formEl.addEventListener('submit', onSubmit);
