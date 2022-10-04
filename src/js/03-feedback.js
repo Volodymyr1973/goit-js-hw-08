ac17e08c98247714e384a24615002552d66bbf6c
Volodymyr <104526892+Volodymyr1973@users.noreply.github.com>
2022-10-04 23:41:40 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,5 @@
-console.log('Hello World');
-
 const inputEl = document.querySelector('.feedback-form input');
-console.dir(inputEl);
+console.log(inputEl.value);
 
 const textareaEl = document.querySelector('.feedback-form textarea');
 console.dir(textareaEl);
@@ -16,7 +14,7 @@ let emailIn;
 
 let textIn;
 
-let arr = {};
+let obj = {};
 
 const onInput = event => {
   event.preventDefault();
@@ -24,34 +22,27 @@ const onInput = event => {
   console.log(event.path[0].type);
   if (event.path[0].type === 'email') {
     emailIn = event.target.value;
-    arr.email = emailIn;
+    obj.email = emailIn;
 
     console.log(emailIn);
   } else if (event.path[0].type === 'textarea') {
     textIn = event.target.value;
-    arr.message = textIn;
+    obj.message = textIn;
 
     console.log(textIn);
   }
 
-  //   let arr = { email: `${emailIn}`, text: `${textIn}` };
-  console.log(arr);
-  localStorage.setItem('feedback-form-state', JSON.stringify(arr));
+  console.log(obj);
+  localStorage.setItem('feedback-form-state', JSON.stringify(obj));
+
+  console.log(localStorage.getItem('feedback-form-state'));
 };
 
 formEl.addEventListener('input', onInput);
 
 const onSubmit = event => {
   event.preventDefault();
-  console.log(event.currentTarget);
-  const email = event.target[0].value;
-  console.dir(email);
-  const textarea = event.target[1].value;
-  console.log(textarea);
-  let arr = { email: `${email}`, text: `${textarea}` };
-  console.log(arr);
-  localStorage.setItem('feedback-form-state', JSON.stringify(arr));
-  event.currentTarget.reset();
+  event.target.reset();
   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   localStorage.removeItem('feedback-form-state');
 };
